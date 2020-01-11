be0a740fec52ce3c2d377f5be71777a61849a558
donnjonn <44286455+donnjonn@users.noreply.github.com>
2020-01-11 20:42:35 +0100

    Update computer_db.sql

--- computer_db.sql
+++ computer_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 08, 2019 at 06:14 PM
--- Server version: 10.1.40-MariaDB
--- PHP Version: 7.3.5
+-- Gegenereerd op: 11 jan 2020 om 20:41
+-- Serverversie: 10.4.8-MariaDB
+-- PHP-versie: 7.3.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `computers`
+-- Tabelstructuur voor tabel `computers`
 --
 
 CREATE TABLE `computers` (
@@ -40,19 +40,20 @@ CREATE TABLE `computers` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `computers`
+-- Gegevens worden geëxporteerd voor tabel `computers`
 --
 
 INSERT INTO `computers` (`c_id`, `c_naam`, `c_omsch`, `c_lok`, `c_opl`, `c_serie`, `c_aankoop`, `c_huur`) VALUES
 (1, 'DESKTOP-1111', 'AMD Ryzen 2700, Nvidia Quadro p1000, 16GB RAM', 'A213', 'E/ICT', 1111111111, 2000, 5),
 (2, 'DESKTOP-1112', 'AMD Ryzen 2700, Nvidia Quadro p1000, 16GB RAM', 'A213', 'E/ICT', 1111111112, 2000, 10),
 (3, 'DESKTOP-1113', 'Intel i7 4790k, Nvidia GTX 980, 16GB RAM', 'B104', 'Chemie', 1111111113, 1250, 5),
-(4, 'DESKTOP-1114', 'Intel i5 9600k, Nvidia Quadro P2000', 'A213', 'E/ICT', 1111111114, 1750, 2);
+(4, 'DESKTOP-1114', 'Intel i5 9600k, Nvidia Quadro P2000', 'A213', 'E/ICT', 1111111114, 1750, 2),
+(5, 'DESKTOP-1116', 'AMD', 'A212', 'E/ICT', 8, 2000, 5);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `groups`
+-- Tabelstructuur voor tabel `groups`
 --
 
 CREATE TABLE `groups` (
@@ -61,7 +62,7 @@ CREATE TABLE `groups` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `groups`
+-- Gegevens worden geëxporteerd voor tabel `groups`
 --
 
 INSERT INTO `groups` (`u_id`, `g_groep`) VALUES
@@ -73,7 +74,7 @@ INSERT INTO `groups` (`u_id`, `g_groep`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `momenten`
+-- Tabelstructuur voor tabel `momenten`
 --
 
 CREATE TABLE `momenten` (
@@ -85,19 +86,22 @@ CREATE TABLE `momenten` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `momenten`
+-- Gegevens worden geëxporteerd voor tabel `momenten`
 --
 
 INSERT INTO `momenten` (`m_id`, `m_van`, `m_tot`, `m_comp`, `m_res`) VALUES
-(1, '2019-12-18 11:00:00', '2019-12-18 12:00:00', 1, 0),
+(1, '2019-12-18 11:00:00', '2019-12-18 12:00:00', 1, 3),
 (2, '2019-12-19 14:00:00', '2019-12-19 15:00:00', 2, 2),
-(3, '2019-12-17 16:00:00', '2019-12-17 17:00:00', 1, 0),
-(4, '2019-12-13 12:00:00', '2019-12-13 14:00:00', 1, 0);
+(3, '2019-12-17 16:00:00', '2019-12-17 17:00:00', 1, 4),
+(4, '2019-12-13 12:00:00', '2019-12-13 14:00:00', 1, 5),
+(5, '0020-12-05 12:00:00', '0020-12-05 13:00:00', 1, 6),
+(6, '2020-05-18 15:00:00', '2020-05-18 16:00:00', 4, 0),
+(7, '2020-06-11 09:00:00', '2020-06-11 10:00:00', 5, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `reservaties`
+-- Tabelstructuur voor tabel `reservaties`
 --
 
 CREATE TABLE `reservaties` (
@@ -107,17 +111,21 @@ CREATE TABLE `reservaties` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `reservaties`
+-- Gegevens worden geëxporteerd voor tabel `reservaties`
 --
 
 INSERT INTO `reservaties` (`r_id`, `r_prijs`, `r_user`) VALUES
 (0, 0, '0'),
-(2, 0, 's1111');
+(2, 0, 's1111'),
+(3, 0, 's1111'),
+(4, 0, 's1111'),
+(5, 10, 'e'),
+(6, 5, 'e');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Tabelstructuur voor tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -128,34 +136,34 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `users`
+-- Gegevens worden geëxporteerd voor tabel `users`
 --
 
 INSERT INTO `users` (`u_id`, `u_naam`, `u_wachtwoord`, `u_richting`) VALUES
 ('0', 'Niet gereserveerd', 'NVT', 'NVT'),
 ('d1111', 'Joost Vennekens', '1234', 'E/ICT'),
 ('d1112', 'Raf Dewil', '4321', 'Chemie'),
-('e', 'Extern', 'NVT', 'ext'),
+('e', 'Extern', '', 'ext'),
 ('s1111', 'Jonas Michiels', 'abcd', 'E/ICT');
 
 --
--- Indexes for dumped tables
+-- Indexen voor geëxporteerde tabellen
 --
 
 --
--- Indexes for table `computers`
+-- Indexen voor tabel `computers`
 --
 ALTER TABLE `computers`
   ADD PRIMARY KEY (`c_id`);
 
 --
--- Indexes for table `groups`
+-- Indexen voor tabel `groups`
 --
 ALTER TABLE `groups`
   ADD KEY `g_uid` (`u_id`);
 
 --
--- Indexes for table `momenten`
+-- Indexen voor tabel `momenten`
 --
 ALTER TABLE `momenten`
   ADD PRIMARY KEY (`m_id`),
@@ -163,37 +171,37 @@ ALTER TABLE `momenten`
   ADD KEY `m_res` (`m_res`);
 
 --
--- Indexes for table `reservaties`
+-- Indexen voor tabel `reservaties`
 --
 ALTER TABLE `reservaties`
   ADD PRIMARY KEY (`r_id`),
   ADD KEY `r_user` (`r_user`);
 
 --
--- Indexes for table `users`
+-- Indexen voor tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`u_id`);
 
 --
--- Constraints for dumped tables
+-- Beperkingen voor geëxporteerde tabellen
 --
 
 --
--- Constraints for table `groups`
+-- Beperkingen voor tabel `groups`
 --
 ALTER TABLE `groups`
   ADD CONSTRAINT `groups_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `users` (`u_id`);
 
 --
--- Constraints for table `momenten`
+-- Beperkingen voor tabel `momenten`
 --
 ALTER TABLE `momenten`
   ADD CONSTRAINT `momenten_ibfk_1` FOREIGN KEY (`m_comp`) REFERENCES `computers` (`c_id`),
   ADD CONSTRAINT `momenten_ibfk_2` FOREIGN KEY (`m_res`) REFERENCES `reservaties` (`r_id`);
 
 --
--- Constraints for table `reservaties`
+-- Beperkingen voor tabel `reservaties`
 --
 ALTER TABLE `reservaties`
   ADD CONSTRAINT `reservaties_ibfk_1` FOREIGN KEY (`r_user`) REFERENCES `users` (`u_id`);
